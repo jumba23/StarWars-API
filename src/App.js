eaa70c0805acfd245e54037234321e1cbe603c78
Goran SD <g.cvetic23@gamail.com>
2022-10-16 19:42:29 -0700

    deploy route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Header setSearchTerm={setSearchTerm} />
-      {pathname === "/" || pathname === "/StarWars-API" ? <LandingPage /> : <Main searchTerm={searchTerm} />}
+      {pathname === "/" || pathname === "/StarWars-API/" ? <LandingPage /> : <Main searchTerm={searchTerm} />}
     </>
   );
 }
